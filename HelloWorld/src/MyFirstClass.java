0e16b9398a63aaee499727e83fb5d681caabfdb6
Veena Saini <veena.sainiaug2shine@gmail.com>
2017-07-27 14:39:04 +0530

    merge

--- HelloWorld/src/MyFirstClass.java
+++ HelloWorld/src/MyFirstClass.java
@@ -2,7 +2,12 @@
 public class MyFirstClass {
 	public static void main(String args[]){
 		System.out.println("Hello Veena!");
+<<<<<<< HEAD
 		System.out.println("Master child 2 ");
+=======
+		System.out.println("bye Veena!");
+		
+>>>>>>> refs/heads/MChild1
 	}
 
 }
